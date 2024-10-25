5ad69616361011c32506d4943288adfbb3229219
fabricio roberts <robertsfabricio68@gmail.com>
2024-10-24 22:05:23 -0300

    funcion-agregar-elemento

--- index.js
+++ index.js
@@ -12,31 +12,35 @@ alert("Ya no hay espacio, estas son las cosas que cargaste: " + cosas_cargadas)
 administrarMochila();
 function administrarMochila(){
     while(mochila_abierta == true){
-        let opcion =prompt("¿Estas satisfecho con el contenido de tu mochila?\n1-Quitar algo\n2-Agregar algo\n3-Cerrar mochila")
+        let opcion =prompt("¿Estas satisfecho con el contenido de tu mochila?\n1-Quitar algo\n2-Agregar algo\n3-Mirar mochila\n4-Cerrar mochila")
         opciones(opcion);
     }
 }
 function quitarObjeto(){
-    let quitar = prompt("¿Que objeto quieres quitar?\n" + cosas_cargadas)
+    let quitar = prompt("¿Que cosa quieres quitar?\n" + cosas_cargadas)
     for(let i = mochila.espacio;i<=5;i++)
     {
         if(cosas_cargadas[i] == quitar){
             cosas_cargadas.splice(i,1)
-            alert("objeto eliminado")
+            alert("Cosa eliminada")
             break;
         }
         else if(i == 5){
-            alert("Objeto no encontrado");
+            alert("Cosa no encontrada");
         }
         else{
-            alert("Buscando objeto");
+            alert("Revisando mochila...");
         }
     }
 }
 function agregarObjeto(){
-
+    let agregar = prompt("¿Que cosa quieres agregar?")
+    cosas_cargadas.length>=5?alert("Mochila llena"):cosas_cargadas.push(agregar) + alert(agregar + " agregado a tu mochila");
 }
 function cerrarMochila(){
+
+}
+function mirarMochila(){
 
 }
 function opciones(opcion){
@@ -49,6 +53,9 @@ function opciones(opcion){
         agregarObjeto();
         break;
         case "3":
+        mirarMochila();
+        break;
+        case "4":
         cerrarMochila();
         break;
 
