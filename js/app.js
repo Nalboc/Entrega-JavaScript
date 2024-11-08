b93fc5d6fe53f219e2840f83c5ef69a4d274a811
fabricio roberts <robertsfabricio68@gmail.com>
2024-11-07 21:23:23 -0300

    DOM-inicio

---
+++ js/app.js
@@ -0,0 +1,72 @@
+const mochila = document.getElementById("mochila");
+const slot1 = document.getElementById("slot1");
+const slot2 = document.getElementById("slot2");
+const slot3 = document.getElementById("slot3");
+const slot4 = document.getElementById("slot4");
+const slot5 = document.getElementById("slot5");
+mochila_abierta = true;
+mochila = {
+  espacio: 5,
+};
+let cosas_cargadas = [];
+while (mochila.espacio > 0) {
+  let cargar_mochila = prompt(
+    "Llena la mochila con lo que quieras\n Espacio de mochila: " +
+      mochila.espacio
+  );
+  mochila.espacio -= 1;
+  cosas_cargadas.push(cargar_mochila);
+}
+alert("Ya no hay espacio, estas son las cosas que cargaste: " + cosas_cargadas);
+administrarMochila();
+function administrarMochila() {
+  while (mochila_abierta == true) {
+    let opcion = prompt(
+      "¿Estas satisfecho con el contenido de tu mochila?\n1-Quitar algo\n2-Agregar algo\n3-Mirar mochila\n4-Cerrar mochila"
+    );
+    opciones(opcion);
+  }
+}
+function quitarObjeto() {
+  let quitar = prompt("¿Que cosa quieres quitar?\n" + cosas_cargadas);
+  for (let i = mochila.espacio; i <= 5; i++) {
+    if (cosas_cargadas[i] == quitar) {
+      cosas_cargadas.splice(i, 1);
+      alert("Cosa eliminada");
+      break;
+    } else if (i == 5) {
+      alert("Cosa no encontrada");
+    } else {
+      alert("Revisando mochila...");
+    }
+  }
+}
+function agregarObjeto() {
+  let agregar = prompt("¿Que cosa quieres agregar?");
+  cosas_cargadas.length >= 5
+    ? alert("Mochila llena")
+    : cosas_cargadas.push(agregar) + alert(agregar + " agregado a tu mochila");
+}
+function cerrarMochila() {
+  alert("Mochila cerrada");
+  mochila_abierta = false;
+}
+function mirarMochila() {
+  alert("Estas son las cosas dentro de tu mochila: " + cosas_cargadas);
+}
+function opciones(opcion) {
+  switch (opcion) {
+    case "1":
+      quitarObjeto();
+      break;
+    case "2":
+      agregarObjeto();
+      break;
+    case "3":
+      mirarMochila();
+      break;
+    case "4":
+      cerrarMochila();
+      break;
+  }
+}
