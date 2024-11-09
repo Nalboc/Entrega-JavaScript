927b9df0c6f6f86812b32fd7532aeef377a06b6d
fabricio roberts <robertsfabricio68@gmail.com>
2024-11-09 00:07:48 -0300

    eventlistener-iniciarsesion-completo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
 const botonIniciarSesion = document.getElementById("iniciarSesion");
-console.log(botonIniciarSesion);
+console.log(document.body.innerHTML);
 botonIniciarSesion.addEventListener("mousedown", () => {
-  console.log("Tocaste iniciar sesion");
+  document.body.innerHTML = `<header>
+        <h1>Inicia sesion</h1>
+    </header>
+    <main>
+        <form class = "formulario">
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre" id="nombre" required>
+            <br/><br/>
+            <label for="contraseña">Contraseña</label>
+            <input type="password" name="contraseña" id="contraseña" required>
+            <br/><br/>
+            <button type=""submit>Enviar</button>
+        </form>
+    </main>
+    <footer>
+
+    </footer>
+    <script src="./js/app.js"></script>`;
 });
